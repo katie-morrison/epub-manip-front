faa14ac93c877a5abd2bf1930d11f0692299158c
katie-morrison <katie.morrison9823@gmail.com>
2025-08-07 22:43:10 +0000

    adding files for github actions

---
+++ tests/basic.test.js
@@ -0,0 +1,7 @@
+describe('simple test to play with github actions', () => {
+    it('The strings should equal', () => {
+        const first = 'Hello World!'
+        const second = 'Hello World!'
+        expect(first).toBe(second)
+    })
+})
